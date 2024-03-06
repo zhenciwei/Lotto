4e46a487ec143dc60778bdbc4dad9f0e09f80d98
zhenciwei <janice900123@gmail.com>
2024-03-06 16:21:48 +0800

    第一次 Commit

---
+++ app/src/main/java/tw/edu/pu/s1111187/lotto/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.s1111187.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
